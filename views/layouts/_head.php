91ea7fe78cbbf57cc32075e6d55b9310256d5054
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-05-23 12:47:13 +0600

    Favicon

--- views/layouts/_head.php
+++ views/layouts/_head.php
@@ -9,36 +9,11 @@ use app\assets\AppAsset;
 AppAsset::register($this);
 
 $this->registerCsrfMetaTags();
-$this->registerMetaTag(
-    ['charset' => Yii::$app->charset],
-    'charset',
-);
-$this->registerMetaTag(
-    [
-        'name' => 'viewport',
-        'content' => 'width=device-width, initial-scale=1',
-    ],
-);
-if (!empty($this->params['meta_description'])) {
-    $this->registerMetaTag(
-        [
-            'name' => 'description',
-            'content' => $this->params['meta_description'],
-        ],
-    );
-}
-if (!empty($this->params['meta_keywords'])) {
-    $this->registerMetaTag(
-        [
-            'name' => 'keywords',
-            'content' => $this->params['meta_keywords'],
-        ],
-    );
-}
-$this->registerLinkTag(
-    [
-        'rel' => 'icon',
-        'type' => 'image/x-icon',
-        'href' => Yii::getAlias('@web/favicon.ico'),
-    ],
-);
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '180x180', 'href' => Yii::getAlias('@web/apple-touch-icon.png')]);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'sizes' => '32x32', 'href' => Yii::getAlias('@web/favicon-32x32.png')]);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'sizes' => '16x16', 'href' => Yii::getAlias('@web/favicon-16x16.png')]);
+$this->registerLinkTag(['rel' => 'manifest', 'href' => Yii::getAlias('@web/site.webmanifest')]);
