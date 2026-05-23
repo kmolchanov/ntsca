9d0e76b688c71e9e1cb58d11084779eeff39b697
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-05-23 11:57:00 +0600

    params

--- config/params.php
+++ config/params.php
@@ -1,4 +1,25 @@
 <?php
 
 return [
+    'languages' => [
+        'ru' => [
+            'label' => 'Русский',
+            'flag' => 'ru',
+            'locale' => 'ru-RU',
+            'appName' => 'Школа Нового Мышления в Центральной Азии',
+        ],
+        'en' => [
+            'label' => 'English',
+            'flag' => 'us',
+            'locale' => 'en-US',
+            'appName' => 'New Thinking School of Central Asia',
+        ],
+        'ky' => [
+            'label' => 'Кыргызча',
+            'flag' => 'kg',
+            'locale' => 'ky-KG',
+            'appName' => 'Борбордук Азиядагы Жаңы Ой Жүгүртүү Мектеби',
+        ],
+    ],
+    'defaultLanguage' => 'ru',
 ];
