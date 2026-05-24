16c4cbb5dfb4362ce517db4762e933f4973e7ce7
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-05-24 19:38:25 +0600

    Footer

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -6,36 +6,26 @@ declare(strict_types=1);
 
 use yii\helpers\Html;
 
+$lang = Yii::$app->params['lang'] ?? Yii::$app->params['defaultLanguage'];
+$language = Yii::$app->params['languages'][$lang] ?? [];
+
 ?>
-<footer id="footer" class="mt-auto py-3 bg-body-tertiary">
+<footer id="footer" class="mt-auto py-4 bg-body-tertiary border-top">
     <div class="container">
-        <div class="row text-body-secondary">
-            <div class="col-md-6 text-center text-md-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end">
-                <a href="https://www.yiiframework.com/" rel="external" class="text-body-secondary text-decoration-none">
-                    <?= Yii::t(
-                        'yii',
-                        'Powered by {yii}',
-                        ['yii' => ''],
-                    ) ?>
-                    <?= Html::img(
-                        '@web/images/yii3_full_for_light.svg',
-                        [
-                            'alt' => 'Yii Framework',
-                            'class' => 'align-text-bottom footer-logo-light',
-                            'height' => '28',
-                        ],
-                    ) ?>
-                    <?= Html::img(
-                        '@web/images/yii3_full_for_dark.svg',
-                        [
-                            'alt' => 'Yii Framework',
-                            'class' => 'align-text-bottom footer-logo-dark',
-                            'height' => '28',
-                        ],
-                    ) ?>
-                </a>
+
+        <div class="text-center text-body-secondary small">
+
+            <div class="fw-semibold mb-2">
+                &copy; <?= Html::encode($language['appName'] ?? Yii::$app->name) ?> <?= date('Y') ?>
             </div>
+
+            <?php if (!empty($language['license'])): ?>
+                <div style="max-width: 900px; margin: 0 auto;">
+                    <?= Html::encode($language['license']) ?>
+                </div>
+            <?php endif; ?>
+
         </div>
+
     </div>
 </footer>
