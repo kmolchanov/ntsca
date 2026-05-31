384445afd9e0c2016e5e6d3c5a752b7b891c4a94
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-05-31 15:38:59 +0600

    Permissions fixes

--- permissions.sh
+++ permissions.sh
@@ -7,10 +7,10 @@ printf "chmod 777 web/assets/\n"
 chmod 755 yii
 printf "chmod 755 yii\n"
 chmod 775 web/uploads/images/cache/
-printf "chmod 777 web/uploads/images/cache/\n"
+printf "chmod 775 web/uploads/images/cache/\n"
 chmod 775 web/uploads/images/store/
-printf "chmod 777 web/uploads/images/store/\n"
+printf "chmod 775 web/uploads/images/store/\n"
 chmod 775 web/uploads/news/
-printf "chmod 777 web/uploads/news/\n"
+printf "chmod 775 web/uploads/news/\n"
 chmod 775 web/uploads/pages/
-printf "chmod 777 web/uploads/pages/\n"
+printf "chmod 775 web/uploads/pages/\n"
