e78d97180f42963411605b82cc21093161705a69
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-06-21 18:32:12 +0600

    Improvements

--- views/site/_video.php
+++ views/site/_video.php
@@ -3,20 +3,13 @@
 /** @var app\models\Video $model */
 
 use app\models\Video;
-use yii\helpers\Html;
 
-$title = $model->title;
 $description = $model->description;
 $player = $model->youtubePlayerHtml;
 ?>
 
 <?php if ($player): ?>
     <section class="page-video my-5">
-
-        <?php if ($title): ?>
-            <h2 class="text-center mb-4"><?= Html::encode($title) ?></h2>
-        <?php endif; ?>
-
         <?php if ($model->description_position == Video::DESCRIPTION_POSITION_TOP && $description): ?>
             <div class="mb-4">
                 <?= $description ?>
