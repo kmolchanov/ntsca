87051ced5330b7d33e90f7f793a588c7a34db267
Konstantin Molchanov <kostya.molchanov@gmail.com>
2026-07-10 23:53:41 +0600

    Navbar - 1 floor now

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -21,63 +21,17 @@ $items = Page::getMenuItems();
 ?>
 <header id="header">
     <nav class="site-navbar fixed-top" aria-label="Main navigation">
-        <div class="site-navbar-top">
-            <div class="container">
-                <div class="site-navbar-top-inner">
-                    <?= Html::a(
-                        Html::img('/images/logo.png', [
-                            'alt' => Yii::$app->name,
-                            'class' => 'header-logo',
-                        ]),
-                        ['/site/index', 'lang' => Yii::$app->params['lang'] ?? 'ru'],
-                        ['class' => 'site-navbar-brand'],
-                    ) ?>
+        <div class="container-fluid">
+            <div class="site-navbar-inner">
+                <?= Html::a(
+                    Html::img('/images/logo.png', [
+                        'alt' => Yii::$app->name,
+                        'class' => 'header-logo',
+                    ]),
+                    ['/site/index', 'lang' => Yii::$app->params['lang'] ?? 'ru'],
+                    ['class' => 'site-navbar-brand'],
+                ) ?>
 
-                    <div class="site-navbar-actions">
-                        <?php if ($current && $languages): ?>
-                            <div class="dropdown site-navbar-language">
-                                <a class="site-navbar-language-toggle dropdown-toggle d-flex align-items-center gap-2"
-                                   href="#"
-                                   id="languageDropdown"
-                                   role="button"
-                                   data-bs-toggle="dropdown"
-                                   aria-expanded="false">
-                                    <span class="fi fi-<?= Html::encode($current['flag']) ?>"></span>
-                                    <span><?= Html::encode($current['label']) ?></span>
-                                </a>
-
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown">
-                                    <?php foreach ($languages as $code => $language): ?>
-                                        <?php $params['lang'] = $code; ?>
-
-                                        <li>
-                                            <a class="dropdown-item d-flex align-items-center gap-2 <?= $code === $currentLang ? 'active' : '' ?>"
-                                               href="<?= Url::to(array_merge([$route], $params)) ?>">
-                                                <span class="fi fi-<?= Html::encode($language['flag']) ?>"></span>
-                                                <span><?= Html::encode($language['label']) ?></span>
-                                            </a>
-                                        </li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            </div>
-                        <?php endif; ?>
-
-                        <button class="navbar-toggler site-navbar-toggler"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#siteNavbarMenu"
-                                aria-controls="siteNavbarMenu"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="site-navbar-menu-row">
-            <div class="container">
                 <div class="collapse navbar-collapse site-navbar-collapse" id="siteNavbarMenu">
                     <?= Nav::widget(
                         [
@@ -88,6 +42,46 @@ $items = Page::getMenuItems();
                             'activateParents' => false,
                         ],
                     ) ?>
+                </div>
+
+                <div class="site-navbar-actions">
+                    <?php if ($current && $languages): ?>
+                        <div class="dropdown site-navbar-language">
+                            <a class="site-navbar-language-toggle dropdown-toggle d-flex align-items-center gap-2"
+                               href="#"
+                               id="languageDropdown"
+                               role="button"
+                               data-bs-toggle="dropdown"
+                               aria-expanded="false">
+                                <span class="fi fi-<?= Html::encode($current['flag']) ?>"></span>
+                                <span><?= Html::encode($current['label']) ?></span>
+                            </a>
+
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown">
+                                <?php foreach ($languages as $code => $language): ?>
+                                    <?php $params['lang'] = $code; ?>
+
+                                    <li>
+                                        <a class="dropdown-item d-flex align-items-center gap-2 <?= $code === $currentLang ? 'active' : '' ?>"
+                                           href="<?= Url::to(array_merge([$route], $params)) ?>">
+                                            <span class="fi fi-<?= Html::encode($language['flag']) ?>"></span>
+                                            <span><?= Html::encode($language['label']) ?></span>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+
+                    <button class="navbar-toggler site-navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#siteNavbarMenu"
+                            aria-controls="siteNavbarMenu"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                 </div>
             </div>
         </div>
